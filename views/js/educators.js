6ce166d0d38eb351e2c5d016c97cca6c8f0d48e1
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2024-04-30 15:50:18 +0300

    Починил попапы на странице администрирования преподавателей

--- views/js/educators.js
+++ views/js/educators.js
@@ -47,57 +47,16 @@ var educators = [
     department: "Литература",
     academicDegree: "к.ф.н",
   },
-  {
-    id: 0,
-    firstName: "Сотников",
-    middleName: "Сергей",
-    lastName: "Викторович",
-    department: "ПМИ",
-    academicDegree: "к.т.н",
-  },
-  {
-    id: 1,
-    firstName: "Тутубалин",
-    middleName: "Павел",
-    lastName: "Иннокентьевич",
-    department: "ПМИ",
-    academicDegree: "к.т.н",
-  },
-  {
-    id: 2,
-    firstName: "Иванов",
-    middleName: "Иван",
-    lastName: "Иванович",
-    department: "ИТ",
-    academicDegree: "д.т.н",
-  },
-  {
-    id: 3,
-    firstName: "Петров",
-    middleName: "Петр",
-    lastName: "Петрович",
-    department: "Биология",
-    academicDegree: "д.б.н",
-  },
-  {
-    id: 4,
-    firstName: "Сидоров",
-    middleName: "Алексей",
-    lastName: "Александрович",
-    department: "История",
-    academicDegree: "к.ф.н",
-  },
-  {
-    id: 5,
-    firstName: "Кузнецова",
-    middleName: "Екатерина",
-    lastName: "Ивановна",
-    department: "Литература",
-    academicDegree: "к.ф.н",
-  },
 ];
 
+var nextId = 6;
 getEducators();
+
+$(document).ready(function () {
+  $(".closePopup").on("click", closeModal);
+
+  $("#createEducatorForm").on("submit", createEducator);
+});
 
 function getEducators() {
   const educatorsElem = $(".educators");
@@ -110,7 +69,7 @@ function getEducators() {
       `<div>${employee.firstName} ${employee.middleName} ${employee.lastName}, ${employee.academicDegree}, ${employee.department}</div>`
     )
       .addClass("educator")
-      .on("click", () => console.log("CLICKED"));
+      .on("click", () => showEditModal(employee.id));
 
     const deleteButton = $("<button>Удалить</button>")
       .addClass("delete")
@@ -126,6 +85,20 @@ function deleteEducator(id) {
   getEducators();
 }
 
+function createEducator() {
+  const formData = new FormData(this);
+  let newEducator = {
+    id: nextId++,
+    firstName: formData.get("firstName"),
+    middleName: formData.get("middleName"),
+    lastName: formData.get("lastName"),
+    department: formData.get("department"),
+    academicDegree: formData.get("academicDegree"),
+  };
+  educators.append(newEducator);
+  console.log(educators);
+}
+
 function filterEducatorsList(event) {
   let searchTerm = event.target.value;
   educators = educators.filter((employee) =>
@@ -137,14 +110,22 @@ function filterEducatorsList(event) {
 }
 
 function showDeleteModal(id) {
-  $(".overlay").addClass("active");
-  $(".deleteConfirm").click(function () {
+  $(".overlay, .delete-popup").addClass("active");
+  $(".confirm-delete").on("click", () => {
     deleteEducator(id);
-    $(".popups").removeClass("active");
-  });
-  $(".close").click(function () {
-    $(".popups").removeClass("active");
+    closeModal();
   });
 }
 
-function showCreateModal() {}
+function showCreateModal() {
+  $(".overlay, .add-popup").addClass("active");
+}
+
+function closeModal() {
+  $(".overlay").removeClass("active");
+  $(this).parent().removeClass("active");
+}
+
+function showEditModal(id) {
+  $(".overlay, .edit-popup").addClass("active");
+}
