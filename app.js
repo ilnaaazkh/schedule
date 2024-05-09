62984a4368cdbbd06f0ac3c56b201108a15cd39d
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2024-05-09 16:29:02 +0300

    Чето сделал недели две назад не помню уже)

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
 const express = require("express");
+
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.get("/", (_, res) => res.send("Hello world"));
 
-app.listen(3000);
+app.listen(port, () =>
+  console.log(`Сервер запущен по адресу: http://localhost:${port}`)
+);
