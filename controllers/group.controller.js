638f7a03d8ce313f4d472f48f46262e1ad821cc3
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2024-05-10 16:05:00 +0300

    Implement controller methods for crud operations to work with groups

--- controllers/group.controller.js
+++ controllers/group.controller.js
@@ -2,6 +2,8 @@ const Group = require("../models/group");
 
 const getGroups = async (_, res) => {
   try {
+    const groups = await Group.find({});
+    res.status(200).json(groups);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -9,6 +11,12 @@ const getGroups = async (_, res) => {
 
 const getGroupById = async (req, res) => {
   try {
+    const { id } = req.params;
+    const group = await Group.findById(id);
+    if (!group) {
+      return res.status(404).json({ message: "Group not found" });
+    }
+    res.status(200).json(group);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -16,6 +24,8 @@ const getGroupById = async (req, res) => {
 
 const createGroup = async (req, res) => {
   try {
+    const group = await Group.create(req.body);
+    res.status(200).json(group);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -23,6 +33,13 @@ const createGroup = async (req, res) => {
 
 const updateGroup = async (req, res) => {
   try {
+    const { id } = req.params;
+    const group = await Group.findByIdAndUpdate(id, req.body);
+    if (!group) {
+      return res.status(404).json({ message: "Group not found" });
+    }
+    const updatedGroup = await Group.findById(id);
+    res.status(200).json(updatedGroup);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -30,6 +47,12 @@ const updateGroup = async (req, res) => {
 
 const deleteGroup = async (req, res) => {
   try {
+    const { id } = req.params;
+    const group = await Group.findByIdAndDelete(id);
+    if (!group) {
+      return res.status(404).json({ message: "Group not found" });
+    }
+    res.status(200).json({ message: "Group deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
